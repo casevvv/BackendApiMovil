5e86e5767487efa6c1520334a89d368657b3dff6
Jairo Lopez <lopezcaste266@gmail.com>
2023-12-03 23:36:52 -0600

    logica de grupo(beta)

--- controllers/controllerFriend.js
+++ controllers/controllerFriend.js
@@ -48,18 +48,32 @@ exports.getFriends = (req, res) => {
     );
 }
 
-exports.deleteFriends = (req, res) => {
-    const { id } = req.query;
+exports.deleteFriend = (req, res) => {
+    try {
+        const { usuario_usuario, usuario_amigo } = req.query;
 
-    conn.query(
-        "DELETE FROM amigos WHERE usuario_amigo = ?",
-        [id],
-        (error, results) => {
-            if (error) {
-                return res.status(500).json({ error });
-            } else {
-                res.status(200).json({ message: 'Amigo eliminado' });
-            }
+        // Validar datos
+        if (!usuario_usuario || !usuario_amigo) {
+            return res.status(400).json({ error: "Faltan datos de usuario" });
         }
-    );
-}
+
+        conn.query(
+            "DELETE FROM amigos WHERE usuario_usuario = ? AND usuario_amigo = ?",
+            [usuario_usuario, usuario_amigo],
+            async (error, result) => {
+                if (error) {
+                    return res.status(500).json({ error: "Error al eliminar amigo de la base de datos" });
+                } else {
+                    if (result.affectedRows === 0) {
+                        return res.status(404).json({ error: "La relación de amistad no existe" });
+                    } else {
+                        res.status(200).json({ message: "Amigo eliminado exitosamente" });
+                    }
+                }
+            }
+        );
+
+    } catch (e) {
+        return res.status(500).json({ error: "Error interno del servidor" });
+    }
+};
